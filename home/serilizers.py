7ead6b6cdd23b996263e900695b0c1c84dca5c70
Your Name <dashsubham095@gmail.com>
2024-10-03 00:56:33 +0530

    date 02-10-2024 django frame work has done

---
+++ home/serilizers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import *
+
+
+class StudentSerilizer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        # fields = ['name','age']
+        exclude = ['id']
+        # fields = '__all__'
+        
+        
