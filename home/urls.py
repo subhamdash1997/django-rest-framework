ce0851f592dd79fb8dfcd8c162689a3675c75057
subhamdash1997 <subham.webdev1997@gmail.com>
2024-10-04 15:26:23 +0530

    APIview has implemented successfully

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,11 @@ from django.urls import path,include
 from .views import *
 
 urlpatterns = [
-    path('',home),
-    path('student/',post_student),
-    path('update-student/<id>/',update_student),
-    path('delete-student/',delete_student),
-    path('get-books/',get_books),
-    path('get-categories/',get_categories)
+    path('students/',StudentAPI.as_view())
+    # path('',home),
+    # path('student/',post_student),
+    # path('update-student/<id>/',update_student),
+    # path('delete-student/',delete_student),
+    # path('get-books/',get_books),
+    # path('get-categories/',get_categories)
 ]
--- home/views.py
+++ home/views.py
@@ -3,60 +3,112 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
-
-
-@api_view(['GET'])
-def get_categories(request):
-    categories = Category.objects.all()
-    serializer = CategorySerializer(categories,many=True)
-    return Response({'status':200,'payload':serializer.data})
-
-
-@api_view(['GET'])
-def get_books(request):
-    book_objs = Book.objects.all()
-    serializer = BookSerializer(book_objs,many=True)
-    return Response({'status':200,'payload':serializer.data})
+from rest_framework.views import APIView
 
 
 
-
-# Create your views here.
-@api_view(['GET'])
-def home(request):
-    student_objs = Student.objects.all()
-    serializer = StudentSerializer(student_objs,many=True)
-    return Response({'status':200,'payload':serializer.data})
-
-@api_view(['POST'])
-def post_student(request):
-    data = request.data
-    serializer = StudentSerializer(data=data)
-    if not serializer.is_valid():
-        return Response({'status':403,'message':'something went wrong.'})
-    serializer.save()
-    return Response({'status':201,'payload':serializer.data,'message':'saved successfully.'})
-
-@api_view(['PATCH'])
-def update_student(request,id):
-    try:
-        student_obj = Student.objects.get(id=id)
-        serializer = StudentSerializer(student_obj,data=request.data,partial = True)
+class StudentAPI(APIView):
+    def get(self,request):
+        student_objs = Student.objects.all()
+        serializer = StudentSerializer(student_objs,many=True)
+        return Response({'status':200,'payload':serializer.data})
+    
+    def post(self,request):
+        data = request.data
+        serializer = StudentSerializer(data=data)
         if not serializer.is_valid():
             return Response({'status':403,'message':'something went wrong.'})
         serializer.save()
         return Response({'status':201,'payload':serializer.data,'message':'saved successfully.'})
-    except Exception as e:
-        return Response({'status':500,'message':'internal server error.'})
+        
+    def put(self,request):
+        pass
 
-@api_view(['DELETE'])
-def delete_student(request):
-    try:
-        id = request.GET.get('id')
-        student_obj = Student.objects.get(id=id)
-        student_obj.delete()
-        return Response({'status':201,'message':'deleted successfully.'})
-    except Exception as e:
-        return Response({'status':500,'message':'internal server error.'})
+    def patch(self,request):
+        try:
+            student_obj = Student.objects.get(id=id)
+            serializer = StudentSerializer(student_obj,data=request.data,partial = True)
+            if not serializer.is_valid():
+                return Response({'status':403,'message':'something went wrong.'})
+            serializer.save()
+            return Response({'status':201,'payload':serializer.data,'message':'saved successfully.'})
+        except Exception as e:
+            return Response({'status':500,'message':'internal server error.'})
+
+    def delete(self,request):
+        try:
+            id = request.GET.get('id')
+            student_obj = Student.objects.get(id=id)
+            student_obj.delete()
+            return Response({'status':201,'message':'deleted successfully.'})
+        except Exception as e:
+            return Response({'status':500,'message':'internal server error.'})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# @api_view(['GET'])
+# def get_categories(request):
+#     categories = Category.objects.all()
+#     serializer = CategorySerializer(categories,many=True)
+#     return Response({'status':200,'payload':serializer.data})
+
+# @api_view(['GET'])
+# def get_books(request):
+#     book_objs = Book.objects.all()
+#     serializer = BookSerializer(book_objs,many=True)
+#     return Response({'status':200,'payload':serializer.data})
+
+# # Create your views here.
+# @api_view(['GET'])
+# def home(request):
+#     student_objs = Student.objects.all()
+#     serializer = StudentSerializer(student_objs,many=True)
+#     return Response({'status':200,'payload':serializer.data})
+
+# @api_view(['POST'])
+# def post_student(request):
+#     data = request.data
+#     serializer = StudentSerializer(data=data)
+#     if not serializer.is_valid():
+#         return Response({'status':403,'message':'something went wrong.'})
+#     serializer.save()
+#     return Response({'status':201,'payload':serializer.data,'message':'saved successfully.'})
+
+# @api_view(['PATCH'])
+# def update_student(request,id):
+#     try:
+#         student_obj = Student.objects.get(id=id)
+#         serializer = StudentSerializer(student_obj,data=request.data,partial = True)
+#         if not serializer.is_valid():
+#             return Response({'status':403,'message':'something went wrong.'})
+#         serializer.save()
+#         return Response({'status':201,'payload':serializer.data,'message':'saved successfully.'})
+#     except Exception as e:
+#         return Response({'status':500,'message':'internal server error.'})
+
+# @api_view(['DELETE'])
+# def delete_student(request):
+#     try:
+#         id = request.GET.get('id')
+#         student_obj = Student.objects.get(id=id)
+#         student_obj.delete()
+#         return Response({'status':201,'message':'deleted successfully.'})
+#     except Exception as e:
+#         return Response({'status':500,'message':'internal server error.'})
     
     
